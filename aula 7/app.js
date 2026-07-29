d4d8dd13ff6d891ffc59d60695966645ba40656b
Equipamentos Laboratorio <compuni@edu.unifil.br>
2026-07-29 17:40:38 -0300

    Aula 7

---
+++ aula 7/app.js
@@ -0,0 +1,9 @@
+let cores = ["Azul", "Amarelo", "Verde", "Vermelho"]
+
+let pessoa = {
+    nome: "Pedro",
+    idade: 18
+}
+
+console.log(pessoa)
+console.log(`{ Nome: ${pessoa.nome}, Idade: ${pessoa.idade} }`)
